b236015ae8b4915c3c1fb3377685428909ef1a0f
Eshwarsekar <eshwarsekar@hotmail.com>
2023-09-23 11:10:17 +0530

    First assessment commit

---
+++ test/src/test/first.java
@@ -0,0 +1,14 @@
+package test;
+
+public class first {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Sample commit for testing 1");
+		System.out.println("Sample commit for testing 2");
+		System.out.println("Sample commit for testing 3");
+		System.out.println("Current commit testing between two users");
+		
+	}
+
+}
